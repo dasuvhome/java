4ed0d491da1d119f4a2a1b25133d61e6ed5c3149
DASuvorov <dasuvhome@yandex.rue>
2021-06-13 10:19:28 +0400

    firstCommit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        start();
+        gameOver();
+
+    }
+
+    public static void start(){
+               print("Hello");
+
+    }
+
+    public static void gameOver(){
+            print("Good Bye");
+    }
+
+    public static void print(String str){
+        System.out.println(str);
+    }
+}
